53c1038c7551de86d74aa4e44770075b471515cd
blackchayenne <blackchayenne@users.noreply.github.com>
2026-01-24 00:22:13 +0300

    Tune next/image sizes and cache

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     formats: ['image/avif', 'image/webp'],
+    deviceSizes: [360, 640, 768, 1024, 1280, 1536, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 31536000,
   },
 }
 
